1f675d7f3ef3c200a6cb1738f5fc711b588625b9
Rahat47 <djrayhan8@gmail.com>
2021-09-29 21:00:37 +0600

    changed portfolio items hover states and removed contact form

--- src/components/Contact/ContactMe.jsx
+++ src/components/Contact/ContactMe.jsx
@@ -65,7 +65,7 @@ const ContactMe = () => {
                 />
 
                 <Row>
-                    <Col lg={6}>
+                    <Col lg={12}>
                         <Row>
                             <Col md={12}>
                                 <Fade bottom>
@@ -122,72 +122,6 @@ const ContactMe = () => {
                                 </Fade>
                             </Col>
                         </Row>
-                    </Col>
-
-                    <Col lg={6}>
-                        <Fade bottom>
-                            <form
-                                role="form"
-                                onSubmit={e => {
-                                    e.preventDefault();
-                                    console.log("Submitted");
-                                    alert("Submitted");
-                                }}
-                                className="email-form"
-                            >
-                                <div className="row">
-                                    <div className="col-md-6 form-group">
-                                        <input
-                                            type="text"
-                                            name="name"
-                                            className="form-control"
-                                            id="name"
-                                            placeholder="Your Name"
-                                            required
-                                        />
-                                    </div>
-                                    <div className="col-md-6 form-group mt-3 mt-md-0">
-                                        <input
-                                            type="email"
-                                            className="form-control"
-                                            name="email"
-                                            id="email"
-                                            placeholder="Your Email"
-                                            required
-                                        />
-                                    </div>
-                                </div>
-                                <div className="form-group mt-3">
-                                    <input
-                                        type="text"
-                                        className="form-control"
-                                        name="subject"
-                                        id="subject"
-                                        placeholder="Subject"
-                                        required
-                                    />
-                                </div>
-                                <div className="form-group mt-3">
-                                    <textarea
-                                        className="form-control"
-                                        name="message"
-                                        rows="6"
-                                        placeholder="Message"
-                                        required
-                                    ></textarea>
-                                </div>
-                                <div className="my-3">
-                                    <div className="loading">Loading</div>
-                                    <div className="error-message"></div>
-                                    <div className="sent-message">
-                                        Your message has been sent. Thank you!
-                                    </div>
-                                </div>
-                                <div className="text-center">
-                                    <button type="submit">Send Message</button>
-                                </div>
-                            </form>
-                        </Fade>
                     </Col>
                 </Row>
             </Container>
